3000569a0ecad9d1b54d880a504eb5a1d827257d
LenAnderson <Anderson.Len@outlook.com>
2024-05-15 08:11:06 -0400

    update readme updater

--- update_readme.py
+++ update_readme.py
@@ -11,6 +11,8 @@ class Command:
 		self.args:str = ''
 		self.hint:str = ''
 		self.examples:list[str] = []
+		self.named:list[dict] = []
+		self.unnamed:list[dict] = []
 	def __str__(self):
 		return self.cmd
 	def __repr__(self):
@@ -20,6 +22,17 @@ re_group = r'^// GROUP: (.+?)\n?$'
 re_start = r'^SlashCommandParser\.addCommandObject\(SlashCommand\.fromProps\(\{\s*name:\s*\'([^\']+)\',\n$'
 re_hint = r'^\s*helpString:\s*(?P<quote>["\'`])(?P<hint>.*?)(?P=quote),\n$'
 re_mhint = r'^\s*helpString:\s*`\s*\n$'
+re_named = r'^\s*SlashCommandNamedArgument\.fromProps\((\{.*)\n$'
+re_unnamed = r'^\s*SlashCommandArgument\.fromProps\((\{.*)\n$'
+re_arg_stop = r'^\s*(\})\),\n$'
+name = 'name'
+description = 'description'
+typeList = 'typeList'
+isRequired = 'isRequired'
+acceptsMultiple = 'acceptsMultiple'
+defaultValue = 'defaultValue'
+enumList = 'enumList'
+true = 'true'
 
 
 with open(os.path.join(os.path.dirname(__file__), 'index.js'), 'r', encoding='utf-8') as f:
@@ -29,31 +42,71 @@ cmd_list:dict[str,Command] = {}
 cmd:Command = None
 group:str = 'Ungrouped'
 is_help = False
+is_named = False
+is_unnamed = False
+arg = ''
 for line in lines:
 	if re.match(re_group, line):
+		# print('GROUP', line)
 		group = re.sub(re_group, r'\1', line)
 		if group not in cmd_list:
 			cmd_list[group] = []
 	elif re.match(re_start, line):
+		# print('START', line)
 		cmd = Command(re.sub(re_start, r'\1', line))
 		cmd_list[group].append(cmd)
-	elif cmd is not None and re.match(re_hint, line):
+		is_help = False
+		is_named = False
+		is_unnamed = False
+	elif cmd and re.match(re_hint, line):
+		# print('HINT', line)
 		cmd.args = ''
 		m = re.match(re_hint, line)
 		cmd.hint = m['hint']
 		cmd = None
-	elif cmd is not None and re.match(re_mhint, line):
+	elif cmd and re.match(re_mhint, line):
+		# print('MHINT', line)
 		cmd.args = ''
 		cmd.hint = ''
 		is_help = True
 	elif cmd and is_help and line.endswith('`,\n'):
+		# print('END MHINT', line)
 		cmd.hint += line[0:-3]
 		cmd.hint = re.sub(r'\s*<div>\n\s*<strong>Example.+?</div>', '', cmd.hint, flags=re.DOTALL)
 		cmd.hint = re.sub(r'(^|\n)\s+', r'\1', cmd.hint, flags=re.DOTALL)
 		is_help = False
 		cmd = None
 	elif cmd and is_help:
+		# print('HELP', line)
 		cmd.hint += line
+	elif cmd and re.match(re_named, line):
+		# print('ARG N', line)
+		is_named = True
+		arg = re.sub(re_named, r'\1', line)
+	elif cmd and re.match(re_unnamed, line):
+		# print('ARG U', line)
+		is_unnamed = True
+		arg = re.sub(re_unnamed, r'\1', line)
+	elif cmd and (is_named or is_unnamed) and re.match(re_arg_stop, line):
+		# print('END ARG', line)
+		arg += re.sub(re_arg_stop, r'\1', line)
+		arg = re.sub(r'(ARGUMENT_TYPE\.[^,\s\]]+)', r'"\1"', arg, flags=re.DOTALL)
+		arg = re.sub(r'\s*new SlashCommandEnumValue\((.+)\),\n', r'[\1],', arg)
+		arg = re.sub(r'defaultValue:\s*`[^`]+`', r'defaultValue: "..."', arg)
+		if is_named:
+			print(arg)
+			cmd.named.append(eval(arg))
+		if is_unnamed:
+			print(arg)
+			cmd.unnamed.append(eval(arg))
+		is_named = False
+		is_unnamed = False
+	elif cmd and (is_named or is_unnamed):
+		# print('IN ARG', line)
+		arg += line
+	else:
+		# print('NO MATCH', line)
+		pass
 
 shutil.copy(os.path.join(os.path.dirname(__file__), 'README.md'), os.path.join(os.path.dirname(__file__), 'README.bak.md'))
 with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r', encoding='utf-8') as f:
@@ -117,8 +170,31 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'w', encoding='u
 		for cmd in cmd_list[group]:
 			f.write('\n'*4)
 			f.write(f'#### `/{cmd.cmd}`\n')
-			if cmd.args:
-				f.write(f'{cmd.args}\n\n')
+			for arg in cmd.named:
+				f.write(f'- `[{arg["name"]}')
+				if 'enumList' in arg and arg['enumList']:
+					enums = '|'.join([x[0] if type(x) == list else x for x in arg['enumList']])
+					f.write(f'={enums}')
+				else:
+					types = '|'.join([x.split('.')[-1].lower() for x in arg["typeList"]])
+					f.write(f':{types}')
+				f.write(']')
+				if 'isRequired' in arg and arg['isRequired']:
+					f.write('')
+				else:
+					f.write('?')
+				if 'defaultValue' in arg and arg['defaultValue']:
+					f.write(f' = {arg["defaultValue"]}')
+				f.write('`')
+				if 'description' in arg and arg['description']:
+					if 'isRequired' in arg and arg['isRequired']:
+						opt = ''
+					else:
+						opt = '*optional* '
+					f.write(f'  \n {opt}{arg["description"]}')
+				f.write('\n')
+			# if cmd.args:
+			# 	f.write(f'{cmd.args}\n\n')
 			f.write(f'{cmd.hint}\n\n')
 			f.write('##### Examples\n\n')
 			if len(cmd.examples) > 0:
@@ -127,4 +203,4 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'w', encoding='u
 					f.write(ex)
 					f.write('```\n\n')
 			else:
-				f.write('```\nsome code here\n```\n\n')
+				f.write('```stscript\nsome code here\n```\n\n')
